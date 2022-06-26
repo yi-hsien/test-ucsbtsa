b0736c6203c543b83ca5cc0bf58090f0f6ec9c85
yi-hsien <yi-hsien@umail.ucsb.edu>
2022-06-25 22:45:46 -0700

    scrollfailed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navigation, Footer, Contact, Home, Events, Officers} from "./components";
 import "./App.css"
-
 function App(){
+  
   return (
+    // <div className="app">
+    //   <Router basename={process.env.PUBLIC_URL}>
+    //     <Navigation/>
+    //     <Routes>
+    //       <Route path="/" element={<Home/>} />
+    //       <Route path="/events" element={<Events/>} />
+    //       <Route path="/officers" element={<Officers/>}/>
+    //     </Routes>
+    //     <Contact/>
+    //     <Footer/>
+    //   </Router>
+    // </div>
     <div className="app">
-      <Router basename={process.env.PUBLIC_URL}>
         <Navigation/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/events" element={<Events/>} />
           <Route path="/officers" element={<Officers/>}/>
         </Routes>
-      </Router>
-      <Contact/>
-      <Footer/>
+        <Contact/>
+        <Footer/>
     </div>
   );
-
 }
+  
+
+
+
 export default App
