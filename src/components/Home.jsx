0f3e04242ff61f5850d56d13b155e666990d12a7
yi-hsien <yi-hsien@umail.ucsb.edu>
2022-06-27 05:49:54 -0700

    some phone adjust

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import VideoContainer  from "./partials/home/VideoContainer";
 import Heading from "./partials/Heading";
 import About from "./partials/home/About";
+import Upcoming from "./partials/home/Upcoming";
 
 function Home(){
 
     return (
         <div className="home" id="home">
             <VideoContainer/>
+            {/* <Heading txt1="Upcoming" txt2="Event"/>
+            <Upcoming/> */}
             <Heading txt1="About" txt2="Us" />
             <About />
 
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as Home } from "./Home";
 export { default as Events } from "./Events";
 export { default as Officers } from "./Officers"; 
 export { default as Contact } from "./partials/Contact";
+
 // export { default as About } from "./About";
 // export { default as Contact } from "./Contact";
