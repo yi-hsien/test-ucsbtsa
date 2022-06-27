8fd6e728de3054f7df9a0197af704e85b79c3356
yi-hsien <yi-hsien@umail.ucsb.edu>
2022-06-27 07:43:49 -0700

    unfixed bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ function Home(){
     return (
         <div className="home" id="home">
             <VideoContainer/>
-            {/* <Heading txt1="Upcoming" txt2="Event"/>
-            <Upcoming/> */}
+            <Heading txt1="✨Upcoming" txt2="Event"/>
+            <Upcoming/>
             <Heading txt1="About" txt2="Us" />
             <About />
 
